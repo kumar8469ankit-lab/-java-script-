0f742f1f574057af83c403766a9c5606c6214438
kumar8469ankit-lab <kumar8469ankit@gmail.com>
2026-01-26 07:37:58 +0000

    learing variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 14453;
+let accountEmail = "ankit@gmial.com";
+var accountPassword = "12345";
+accountCity = "jaipur";  // prefer not to use 
+
+let accountState; 
+
+
+ //  accountId = 2; not allowed //
+console.log(accountId);
+
+accountEmail = "hchfh@gmail.com";
+
+accountPassword = "489743676";
+
+accountCity = "delhi";
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+
+
