65af5ba3915417c0e6003cfcc6dbda810abc67f1
kumar8469ankit-lab <kumar8469ankit@gmail.com>
2026-01-26 15:43:30 +0000

    string methods

---
+++ 01_basics/04_string.js
@@ -0,0 +1,21 @@
+const name = "ankit";
+const repoCount  = 50;
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('ankitkr')
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+
+const newString = gameName.substring(0,4);
+
+const newStringone = "       hello world   ";
+
+//console.log(newStringone);
+// console.log(newStringone.trim());
+ 
+const url = "https://eduniti%20physics"
+
+console.log(url.replace('%20','and'));
