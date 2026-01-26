5896ec5730cd16de40eebb2f592526bf65014cf8
kumar8469ankit-lab <kumar8469ankit@gmail.com>
2026-01-26 07:55:17 +0000

    data types end

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,4 +1,16 @@
 "use strict";
 
 console.log(3+3);
+ 
+let name = "ankit"
+let age = "19"
 
+let isAdult = false
+
+let state;
+
+
+// null => standalone value
+// undefined => value abhi assign nahi hui hai
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
